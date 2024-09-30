f98e6a5cce0c7f93f152e119fab546d6bdb8fe14
vscing <513335394@qq.com>
2024-09-30 16:56:51 +0800

    Create HelloListDto.java

---
+++ vscing-admin/src/main/java/com/vscing/admin/dto/HelloListDto.java
@@ -0,0 +1,15 @@
+package com.vscing.admin.dto;
+
+import com.vscing.common.domain.BaseEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HelloListDto extends BaseEntity {
+
+  private Integer id;
+
+  private String title;
+
+}
