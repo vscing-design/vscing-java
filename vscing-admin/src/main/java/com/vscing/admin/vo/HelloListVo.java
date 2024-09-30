65e6d42a0583e64409d63c9d71fdc6be7c957d3b
vscing <513335394@qq.com>
2024-09-30 17:00:48 +0800

    Create HelloListVo.java

---
+++ vscing-admin/src/main/java/com/vscing/admin/vo/HelloListVo.java
@@ -0,0 +1,13 @@
+package com.vscing.admin.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HelloListVo {
+
+    private Integer id;
+
+    private String title;
+}
