412deb0fcee1a71774eb6119eed9d6925c85bea6
vscing <513335394@qq.com>
2024-10-16 14:56:12 +0800

    Update User.java

--- vscing-admin/src/main/java/com/vscing/admin/entity/User.java
+++ vscing-admin/src/main/java/com/vscing/admin/entity/User.java
@@ -6,14 +6,33 @@ import com.vscing.common.domain.BaseEntity;
  * @author vscing (vscing@foxmail.com)
  * @date 2024-10-15 23:51:25
 */
+@Getter
+@Setter
+// 使用lombok 来重写toString和
+//@Data
+//@EqualsAndHashCode(callSuper = true)
+//@ToString(callSuper = true)
 public class User extends BaseEntity {
 
     private Long id;
 
+    // 用户名称
     private String username;
 
+    // 用户邮箱
     private String password;
 
+    // 用户手机号
     private String phone;
 
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", userName='" + username + '\'' +
+                ", passWord='" + password + '\'' +
+                ", phone='" + phone + '\'' +
+                super.toString() + // 调用 BaseEntity 的 toString 方法
+                '}';
+    }
 }
