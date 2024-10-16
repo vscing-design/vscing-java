d33a3f665c3c4bfa28df8f1e7549de3f1f787499
vscing <513335394@qq.com>
2024-10-16 20:28:24 +0800

    Update UserServiceImpl.java

--- vscing-admin/src/main/java/com/vscing/admin/service/impl/UserServiceImpl.java
+++ vscing-admin/src/main/java/com/vscing/admin/service/impl/UserServiceImpl.java
@@ -4,8 +4,11 @@ import com.vscing.admin.mapper.UserMapper;
 import com.vscing.admin.service.UserService;
 import com.vscing.admin.vo.UserVo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 
+@Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
