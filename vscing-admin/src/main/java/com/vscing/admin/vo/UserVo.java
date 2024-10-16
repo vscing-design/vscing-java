7331de178cf9f17a67a07b1ae289f41da9f6410a
vscing <513335394@qq.com>
2024-10-16 14:51:18 +0800

    Update UserVo.java

--- vscing-admin/src/main/java/com/vscing/admin/vo/UserVo.java
+++ vscing-admin/src/main/java/com/vscing/admin/vo/UserVo.java
@@ -10,4 +10,33 @@ import lombok.Setter;
 @Getter
 @Setter
 public class UserVo {
+
+  private Long id;
+
+    // 用户名称
+    private String username;
+
+    // 用户邮箱
+    private String password;
+
+    // 用户手机号
+    private String phone;
+
+    /** 创建时间 */
+    private LocalDateTime createdAt;
+
+    /** 创建者 */
+    private String createdBy;
+
+    /** 更新时间 */
+    private LocalDateTime updatedAt;
+
+    /** 更新者 */
+    private String updatedBy;
+
+    /** 删除时间 */
+    private LocalDateTime deletedAt;
+
+    /** 删除者 */
+    private String deletedBy;
 }
