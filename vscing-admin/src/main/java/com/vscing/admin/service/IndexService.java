71e853efdbb940c57c2af55a9cf69f11dc844bce
vscing <513335394@qq.com>
2024-10-16 14:53:24 +0800

    Update and rename IndexService.java to UserService.java

--- vscing-admin/src/main/java/com/vscing/admin/service/IndexService.java
+++
@@ -1,5 +0,0 @@
-package com.vscing.admin.service;
-
-public interface IndexService {
-  // 弄一些查询列表啊，删除啊的方法
-}
---
+++ vscing-admin/src/main/java/com/vscing/admin/service/UserService.java
@@ -0,0 +1,11 @@
+package com.vscing.admin.service;
+
+import com.vscing.admin.vo.UserVo;
+
+import java.util.List;
+
+public interface UserService {
+
+    List<UserVo> getList();
+}
+
