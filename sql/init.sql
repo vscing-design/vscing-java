81bda9c31020cc8729437be651ed4a5e6f26999b
vscing <513335394@qq.com>
2024-10-16 14:37:00 +0800

    Create init.sql

---
+++ sql/init.sql
@@ -0,0 +1,32 @@
+DROP TABLE IF EXISTS vscing_user;
+DROP TABLE IF EXISTS vscing_order;
+
+CREATE TABLE vscing_user (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(20) NOT NULL,
+  password VARCHAR(50) NOT NULL,
+  phone VARCHAR(20) UNIQUE NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  created_by INT,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  updated_by INT,
+  deleted_at TIMESTAMP NULL,
+  deleted_by INT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci comment = '用户信息表';
+
+CREATE TABLE vscing_order (
+  order_id INT AUTO_INCREMENT PRIMARY KEY,
+  user_id INT NOT NULL,
+  amount DECIMAL(10, 2) NOT NULL,
+  status VARCHAR(20) NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  created_by INT,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  updated_by INT,
+  deleted_at TIMESTAMP NULL,
+  deleted_by INT,
+  CONSTRAINT fk_user
+    FOREIGN KEY (user_id)
+    REFERENCES vscing_user(id)
+    ON DELETE CASCADE
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci comment = '订单信息表';
