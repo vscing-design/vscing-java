e75f6029ea9225263cbc27eb2d45e2b29eddb90a
vscing <513335394@qq.com>
2024-10-17 19:30:25 +0800

    Update UserService.java

--- vscing-admin/src/main/java/com/vscing/admin/service/UserService.java
+++ vscing-admin/src/main/java/com/vscing/admin/service/UserService.java
@@ -1,5 +1,6 @@
 package com.vscing.admin.service;
 
+import com.vscing.admin.dto.UserDto;
 import com.vscing.admin.vo.UserVo;
 
 import java.util.List;
@@ -7,5 +8,7 @@ import java.util.List;
 public interface UserService {
 
     List<UserVo> getList();
+
+    int addInfo(UserDto userInfo);
 }
 
