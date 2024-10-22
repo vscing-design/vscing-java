a8ae0abdd02add72ae7744375ab41a4b9fc08113
vscing <513335394@qq.com>
2024-10-22 20:04:02 +0800

    Update UserServiceImpl.java

--- vscing-admin/src/main/java/com/vscing/admin/service/impl/UserServiceImpl.java
+++ vscing-admin/src/main/java/com/vscing/admin/service/impl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.vscing.admin.service.impl;
 
 import com.vscing.admin.dto.UserDto;
+import com.vscing.admin.dto.UserListDto;
 import com.vscing.admin.mapper.UserMapper;
 import com.vscing.admin.service.UserService;
 import com.vscing.admin.vo.UserVo;
@@ -16,12 +17,18 @@ public class UserServiceImpl implements UserService {
     private UserMapper userMapper;
 
     @Override
-    public List<UserVo> getList() {
-        return userMapper.getList();
+    public List<UserVo> getAllList() {
+        return userMapper.getAllList();
     }
 
     @Override
     public int addInfo(UserDto userInfo) {
         return userMapper.addInfo(userInfo);
+    }
+
+    @Override
+    public List<UserVo> getList(UserListDto queryParam, Integer pageSize, Integer pageNum) {
+        pageNum = (pageNum - 1) * pageSize;
+        return userMapper.getList(queryParam, pageSize, pageNum);
     }
 }
