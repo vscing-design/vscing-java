2bf27e6de56171dacdbbe007818c6660b1f7831f
vscing <513335394@qq.com>
2024-10-22 20:04:42 +0800

    Update UserService.java

--- vscing-admin/src/main/java/com/vscing/admin/service/UserService.java
+++ vscing-admin/src/main/java/com/vscing/admin/service/UserService.java
@@ -1,14 +1,16 @@
 package com.vscing.admin.service;
 
 import com.vscing.admin.dto.UserDto;
+import com.vscing.admin.dto.UserListDto;
 import com.vscing.admin.vo.UserVo;
 
 import java.util.List;
 
 public interface UserService {
 
-    List<UserVo> getList();
+    List<UserVo> getAllList();
 
     int addInfo(UserDto userInfo);
-}
 
+    List<UserVo> getList(UserListDto queryParam, Integer pageSize, Integer pageNum);
+}
