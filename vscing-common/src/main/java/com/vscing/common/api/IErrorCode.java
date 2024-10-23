5223e1e1b44b3a966172997cea3395ca60bb98fb
vscing <513335394@qq.com>
2024-10-23 19:26:18 +0800

    Update IErrorCode.java

--- vscing-common/src/main/java/com/vscing/common/api/IErrorCode.java
+++ vscing-common/src/main/java/com/vscing/common/api/IErrorCode.java
@@ -1,16 +1,11 @@
 package com.vscing.common.api;
 
-/**
- * API返回码接口
- */
+// API返回码接口
 public interface IErrorCode {
-  /**
-   * 返回码
-   */
+
+  // 返回码
   long getCode();
 
-  /**
-   * 返回信息
-   */
+  // 返回信息
   String getMessage();
 }
