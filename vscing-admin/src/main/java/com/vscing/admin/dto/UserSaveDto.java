d876494cecafeea57267ea6f2b747d9bc23edb46
vscing <513335394@qq.com>
2024-10-23 19:34:50 +0800

    Create UserSaveDto.java

---
+++ vscing-admin/src/main/java/com/vscing/admin/dto/UserSaveDto.java
@@ -0,0 +1,22 @@
+package com.vscing.admin.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserSaveDto {
+
+    @NotNull(message = "ID不能为空")
+    private Long id;
+
+    @NotNull(message = "用户名不能为空")
+    private String username;
+
+    @NotNull(message = "手机号不能为空")
+    private String phone;
+
+    @NotNull(message = "密码不能为空")
+    private String password;
+}
