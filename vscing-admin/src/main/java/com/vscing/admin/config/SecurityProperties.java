daaa6aa210ce36c7796a8deb9cd39e09effbbf82
vscing <vscing@foxmail.com>
2024-12-14 20:36:14 +0800

    secure.ignored url白名单

---
+++ vscing-admin/src/main/java/com/vscing/admin/config/SecurityProperties.java
@@ -0,0 +1,26 @@
+package com.vscing.admin.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * SecurityProperties
+ *
+ * @date 2024/12/14 16:25
+ * @author vscing(vscing@foxmail.com)
+ */
+@Component
+@ConfigurationProperties(prefix = "secure.ignored")
+@Getter
+@Setter
+public class SecurityProperties {
+
+    /**
+     * 白名单 URL 集合
+     */
+    private List<String> urls;
+}
