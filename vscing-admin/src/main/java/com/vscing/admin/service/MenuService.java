d94aa91e9b20be93cc8231493a19516e75878eb5
vscing <513335394@qq.com>
2024-12-22 10:42:45 +0800

    Create MenuService.java

---
+++ vscing-admin/src/main/java/com/vscing/admin/service/MenuService.java
@@ -0,0 +1,36 @@
+package com.vscing.admin.service;
+
+import com.vscing.model.dto.MenuListDto;
+import com.vscing.model.entity.Menu;
+
+import java.util.List;
+
+public interface MenuService {
+
+    /**
+     * 列表
+     */
+    List<Menu> getList(MenuListDto record, Integer pageSize, Integer pageNum);
+
+    /**
+     * 详情
+     */
+    Menu getDetails(long id);
+
+    /**
+     * 新增
+     */
+    long created(Menu Menu);
+
+    /**
+     * 编辑
+     */
+    long updated(Menu Menu);
+
+    /**
+     * 删除
+     */
+    long deleted(long id, long deleterId);
+
+
+}
