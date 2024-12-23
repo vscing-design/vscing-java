871a9c4f16f4504eb6df16366bfe11822cd8f68d
vscing <vscing@foxmail.com>
2024-12-23 21:56:40 +0800

    定时任务

--- vscing-admin/src/main/java/com/vscing/admin/component/ScheduledTasks.java
+++ vscing-admin/src/main/java/com/vscing/admin/component/ScheduledTasks.java
@@ -5,10 +5,14 @@ import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 
+/** 
+ * @author vscing (vscing@foxmail.com)
+ * @date 2024-12-23 20:23:24
+*/
 @Component
 public class ScheduledTasks {
 
-    @Scheduled(cron = "0/10 * * * * ?") // 每10秒执行一次
+    @Scheduled(cron = "0 0 22,0 * * ?") // 每天晚上10点和午夜12点执行
     public void executeTask() {
         System.out.println("执行定时任务：" + LocalDateTime.now());
     }
