8abf926132e1bdc8abade872cfbdbb6bf9538b18
vscing <513335394@qq.com>
2024-12-23 16:00:53 +0800

    Create OrganizationMapper.java

---
+++ vscing-model/src/main/java/com/vscing/model/mapper/OrganizationMapper.java
@@ -0,0 +1,29 @@
+package com.vscing.model.mapper;
+
+import com.vscing.model.dto.OrganizationListDto;
+import com.vscing.model.entity.Organization;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface OrganizationMapper {
+
+    List<Organization> getList(OrganizationListDto record);
+
+    Organization selectById(long id);
+
+    int insert(Organization record);
+
+    int softDeleteById(@Param("id") long id, @Param("deleterId") long deleterId);
+
+    int update(Organization record);
+
+    List<Organization> getOrganizationsByUserId(long id);
+
+    int deleteOrganizationsByUserId(long id);
+
+    int insertOrganizationsBatch(@Param("id") long id, @Param("organizationIds") List<Long> organizationIds);
+
+}
