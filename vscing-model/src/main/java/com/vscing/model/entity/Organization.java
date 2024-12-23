b243bf513510f6d4a6b62a9bc41a6593e8e347d5
vscing <513335394@qq.com>
2024-12-23 14:31:03 +0800

    Create Organization.java

---
+++ vscing-model/src/main/java/com/vscing/model/entity/Organization.java
@@ -0,0 +1,36 @@
+package com.vscing.model.entity;
+
+import com.vscing.model.domain.BaseEntity;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Organization
+ *
+ * @author vscing
+ * @date 2024/12/22 00:46
+ */
+@Getter
+@Setter
+public class Organization extends BaseEntity {
+
+    @Schema(description = "主键Id")
+    private Long id;
+
+    @Schema(description = "父级Id")
+    private Long parentId;
+
+    @Schema(description = "名称")
+    private String name;
+
+    @Schema(description = "机构类型：1->企业")
+    private Integer type;
+
+    @Schema(description = "排序")
+    private Integer sortOrder;
+
+    @Schema(description = "备注")
+    private String notes;
+
+}
