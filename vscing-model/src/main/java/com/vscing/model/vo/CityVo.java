4f805012007308b25c2eee20dce88026f05d5d88
vscing <vscing@foxmail.com>
2024-12-26 00:21:52 +0800

    feat: 增加vo

---
+++ vscing-model/src/main/java/com/vscing/model/vo/CityVo.java
@@ -0,0 +1,51 @@
+package com.vscing.model.vo;
+
+import com.vscing.model.entity.District;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * City
+ *
+ * @author vscing
+ * @date 2024/12/23 00:40
+ */
+@Data
+public class CityVo {
+
+  @Schema(description = "城市编码")
+  private Long id;
+
+  @Schema(description = "省份编码")
+  private Long provinceId;
+
+  @Schema(description = "海威供应商城市ID")
+  private Long s1CityId;
+
+  @Schema(description = "城市名称")
+  private String name;
+
+  @Schema(description = "城市简称")
+  private String shortName;
+
+  @Schema(description = "城市编码")
+  private String pinyin;
+
+  @Schema(description = "城市首字母")
+  private String firstLetter;
+
+  @Schema(description = "城市首字母全拼")
+  private String fullLetter;
+
+  @Schema(description = "城市经度")
+  private String lng;
+
+  @Schema(description = "城市维度")
+  private String lat;
+
+  @Schema(description = "区县列表")
+  private List<District> children;
+
+}
---
+++ vscing-model/src/main/java/com/vscing/model/vo/ProvinceVo.java
@@ -0,0 +1,45 @@
+package com.vscing.model.vo;
+
+import com.vscing.model.entity.City;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * Province
+ *
+ * @author vscing
+ * @date 2024/12/23 00:36
+ */
+@Data
+public class ProvinceVo {
+
+  @Schema(description = "省份编码")
+  private Long id;
+
+  @Schema(description = "省份名称")
+  private String name;
+
+  @Schema(description = "省份简称")
+  private String shortName;
+
+  @Schema(description = "省份编码")
+  private String pinyin;
+
+  @Schema(description = "省份首字母")
+  private String firstLetter;
+
+  @Schema(description = "省份首字母全拼")
+  private String fullLetter;
+
+  @Schema(description = "省份经度")
+  private String lng;
+
+  @Schema(description = "省份维度")
+  private String lat;
+
+  @Schema(description = "城市列表")
+  private List<City> children;
+
+}
