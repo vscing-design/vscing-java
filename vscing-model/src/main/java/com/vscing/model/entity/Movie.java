f4be32f3ddd9e37a36fe53e8ca3a41fb5bd0c1b5
vscing <513335394@qq.com>
2024-12-27 14:25:30 +0800

    Update Movie.java

--- vscing-model/src/main/java/com/vscing/model/entity/Movie.java
+++ vscing-model/src/main/java/com/vscing/model/entity/Movie.java
@@ -18,31 +18,58 @@ import java.time.LocalDateTime;
 public class Movie extends BaseEntity {
 
   @Schema(description = "主键ID")
-  private Long id;
+    private Long id;
 
-  @Schema(description = "三方ID")
-  private Long movieId;
+    @Schema(description = "三方ID")
+    private Long movieId;
 
-  @Schema(description = "影院ID")
-  private Long cinemaId;
+    @Schema(description = "影院ID")
+    private Long cinemaId;
 
-  @Schema(description = "影院名称")
-  private String cinemaName;
+    @Schema(description = "影院名称")
+    private String cinemaName;
 
-  @Schema(description = "影片名称")
-  private String name;
+    @Schema(description = "影片名称")
+    private String name;
 
-  @Schema(description = "影片时长（分钟）")
-  private Integer duration;
+    @Schema(description = "影片时长（分钟）")
+    private Integer duration;
 
-  @Schema(description = "上映日期")
-  private LocalDateTime publishDate;
+    @Schema(description = "上映日期")
+    private LocalDateTime publishDate;
 
-  @Schema(description = "影片导演")
-  private String director;
+    @Schema(description = "影片导演")
+    private String director;
 
-  @Schema(description = "影片主演")
-  private String cast;
+    @Schema(description = "影片主演")
+    private String cast;
+
+    @Schema(description = "影片简介")
+    private String intro;
+
+    @Schema(description = "上映类型")
+    private String versionType;
+
+    @Schema(description = "影片语言")
+    private String language;
+
+    @Schema(description = "影片类型")
+    private String movieType;
+
+    @Schema(description = "海报图片")
+    private String posterUrl;
+
+    @Schema(description = "剧情照，多个用英文逗号隔开")
+    private String plotUrl;
+
+    @Schema(description = "评分")
+    private String grade;
+
+    @Schema(description = "想看人数")
+    private Integer like;
+
+    @Schema(description = "上映类型，HOT为热映，WAIT为待上映")
+    private Integer publishStatus;
 
 
 
