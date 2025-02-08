34b11ca1c61e09d55dd6d76fa23e2928b3f1f9b5
vscing <vscing@foxmail.com>
2025-02-08 23:51:39 +0800

    feat: 增加mq延迟队列

--- vscing-mq/src/main/java/com/vscing/mq/config/RabbitMQConfig.java
+++ vscing-mq/src/main/java/com/vscing/mq/config/RabbitMQConfig.java
@@ -23,18 +23,38 @@ import java.util.Map;
 @Configuration
 public class RabbitMQConfig {
 
-    // 延迟交换机名称
+    /**
+     * 延迟交换机名称
+    */
     public static final String DELAYED_EXCHANGE_NAME = "delayed_exchange";
 
-    // 同步场次码相关配置
+    /**
+     * 同步场次码相关配置
+    */
     public static final String SYNC_CODE_QUEUE = "sync_code_queue";
     public static final String SYNC_CODE_ROUTING_KEY = "sync_code_routing_key";
 
-    // 取消订单相关配置
+    /**
+     * 取消订单相关配置
+    */
     public static final String CANCEL_ORDER_QUEUE = "cancel_order_queue";
     public static final String CANCEL_ORDER_ROUTING_KEY = "cancel_order_routing_key";
 
-    // 死信交换机和队列配置
+    /**
+     * 退款相关配置
+    */
+    public static final String REFUND_QUEUE = "refund_queue";
+    public static final String REFUND_ROUTING_KEY = "refund_routing_key";
+
+    /**
+     * 退款订单查询相关配置
+    */
+    public static final String REFUND_QUERY_QUEUE = "refund_query_queue";
+    public static final String REFUND_QUERY_ROUTING_KEY = "refund_query_routing_key";
+
+    /**
+     * 死信交换机和队列配置
+    */
     public static final String DLX_EXCHANGE_NAME = "dlx_exchange";
     public static final String DLX_QUEUE_NAME = "dlx_queue";
     public static final String DLX_ROUTING_KEY = "dlx_routing_key";
@@ -54,9 +74,11 @@ public class RabbitMQConfig {
      */
     @Bean
     public Queue syncCodeQueue() {
-        Map<String, Object> args = new HashMap<>();
-        args.put("x-dead-letter-exchange", DLX_EXCHANGE_NAME); // 指定死信交换机
-        args.put("x-dead-letter-routing-key", DLX_ROUTING_KEY); // 指定死信路由键
+        Map<String, Object> args = new HashMap<>(2);
+        // 指定死信交换机
+        args.put("x-dead-letter-exchange", DLX_EXCHANGE_NAME);
+        // 指定死信路由键
+        args.put("x-dead-letter-routing-key", DLX_ROUTING_KEY);
         return QueueBuilder.durable(SYNC_CODE_QUEUE).withArguments(args).build();
     }
 
@@ -70,15 +92,53 @@ public class RabbitMQConfig {
      */
     @Bean
     public Queue cancelOrderQueue() {
-        Map<String, Object> args = new HashMap<>();
-        args.put("x-dead-letter-exchange", DLX_EXCHANGE_NAME); // 指定死信交换机
-        args.put("x-dead-letter-routing-key", DLX_ROUTING_KEY); // 指定死信路由键
+        Map<String, Object> args = new HashMap<>(2);
+        // 指定死信交换机
+        args.put("x-dead-letter-exchange", DLX_EXCHANGE_NAME);
+        // 指定死信路由键
+        args.put("x-dead-letter-routing-key", DLX_ROUTING_KEY);
         return QueueBuilder.durable(CANCEL_ORDER_QUEUE).withArguments(args).build();
     }
 
     @Bean
     public Binding bindingCancelOrderQueueToDelayedExchange(Queue cancelOrderQueue) {
         return BindingBuilder.bind(cancelOrderQueue).to(delayedExchange()).with(CANCEL_ORDER_ROUTING_KEY).noargs();
+    }
+
+    /**
+     * 创建退款队列，并绑定到延迟交换机。
+     */
+    @Bean
+    public Queue refundQueue() {
+        Map<String, Object> args = new HashMap<>(2);
+        // 指定死信交换机
+        args.put("x-dead-letter-exchange", DLX_EXCHANGE_NAME);
+        // 指定死信路由键
+        args.put("x-dead-letter-routing-key", DLX_ROUTING_KEY);
+        return QueueBuilder.durable(REFUND_QUEUE).withArguments(args).build();
+    }
+
+    @Bean
+    public Binding bindingRefundQueueToDelayedExchange(Queue refundQueue) {
+        return BindingBuilder.bind(refundQueue).to(delayedExchange()).with(REFUND_ROUTING_KEY).noargs();
+    }
+
+    /**
+     * 创建退款订单查询队列，并绑定到延迟交换机。
+     */
+    @Bean
+    public Queue refundQueryQueue() {
+        Map<String, Object> args = new HashMap<>(2);
+        // 指定死信交换机
+        args.put("x-dead-letter-exchange", DLX_EXCHANGE_NAME);
+        // 指定死信路由键
+        args.put("x-dead-letter-routing-key", DLX_ROUTING_KEY);
+        return QueueBuilder.durable(REFUND_QUERY_QUEUE).withArguments(args).build();
+    }
+
+    @Bean
+    public Binding bindingRefundQueryQueueToDelayedExchange(Queue refundQueryQueue) {
+        return BindingBuilder.bind(refundQueryQueue).to(delayedExchange()).with(REFUND_QUERY_ROUTING_KEY).noargs();
     }
 
     /**
