9d26381cdee9ae58efd5e996e79193b588c2b59f
vscing <vscing@foxmail.com>
2025-03-07 01:18:20 +0800

    feat: 调整

---
+++ vscing-api/src/main/java/com/vscing/api/controller/v1/UserEarnController.java
@@ -0,0 +1,28 @@
+package com.vscing.api.controller.v1;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * UserEarnController
+ *
+ * @author vscing
+ * @date 2025/3/7 01:10
+ */
+@Slf4j
+@RestController
+@RequestMapping("/v1/userEarn")
+@Tag(name = "推广费用明细接口", description = "推广费用明细接口")
+public class UserEarnController {
+
+  // 收益明细列表
+
+  // 邀请列表
+
+  // 推广二维码
+
+  // 邀请新人的mq、出票的mq
+
+}
---
+++ vscing-api/src/main/java/com/vscing/api/controller/v1/UserWithdrawController.java
@@ -0,0 +1,26 @@
+package com.vscing.api.controller.v1;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * UserWithdrawController
+ *
+ * @author vscing
+ * @date 2025/3/7 01:11
+ */
+@Slf4j
+@RestController
+@RequestMapping("/v1/userWithdraw")
+@Tag(name = "佣金提现接口", description = "佣金提现接口")
+public class UserWithdrawController {
+
+  // 用户的待提现金额、已提现金额、累积金额
+
+  // 用户发起提现
+
+  // 用户的提现列表
+
+}
